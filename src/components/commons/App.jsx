1d604ae39b9c6666f96406506d46077739e2adab
Slocx <33150302+Slocx@users.noreply.github.com>
2022-01-13 12:59:47 +0100

    :tada: Initial Commit

---
+++ src/components/commons/App.jsx
@@ -0,0 +1,17 @@
+// faire l'import de react
+import React, { Fragment } from "react";
+import './App.css';
+
+// Créer la classe composant
+export class App extends React.Component{
+    render(){
+        return (
+            <Fragment>
+                <h1> Cinéma </h1>
+
+            </Fragment>
+        );
+    };
+};
+
+export default App;
